2c69dd6c79e1a98b828b995313fe34fb72510e72
Austin Drenski <austin.drenski@gmail.com>
2017-01-29 19:21:46 -0500

    Initial commit.

---
+++ src/DbManagementService.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using JetBrains.Annotations;
+
+namespace DatabaseManagementApi
+{
+    [PublicAPI]
+    public abstract class DbManagementService
+    {
+        protected readonly DbContext Context;
+
+        protected readonly IDictionary<string, string> Parameters;
+
+        protected DbManagementService(DbContext context, IDictionary<string, string> parameters = null)
+        {
+            Context = context;
+            Parameters = parameters;
+        }
+
+        public int Execute()
+        {
+            return Invoke();
+        }
+
+        protected abstract int Invoke();
+    }
+}
---
+++ src/DbManagementServiceContainer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using JetBrains.Annotations;
+
+namespace DatabaseManagementApi
+{
+    [PublicAPI]
+    public sealed class DbManagementServiceContainer : IDisposable
+    {
+        private readonly Func<DbContext, IDictionary<string, string>, int> _service;
+
+        public string Name { get; }
+
+        public IDictionary<string, string> Parameters { get; set; }
+
+        public DbManagementServiceContainer(Func<DbContext, IDictionary<string, string>, int> service, string name)
+        {
+            _service = service;
+            Name = name;
+            Parameters = new Dictionary<string, string>();
+        }
+
+        public int Invoke(DbContext context)
+        {
+            return _service(context, Parameters);
+        }
+
+        void IDisposable.Dispose() { }
+    }
+}
