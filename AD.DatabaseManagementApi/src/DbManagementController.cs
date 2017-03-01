126728a2eb1624f00f2dddf58260c51d177f4d8e
Austin Drenski <austin.drenski@gmail.com>
2017-03-01 00:27:14 -0500

    split out a DbManagementControllerBuilder to style after the asp.net core configuration pattern.

--- AD.DatabaseManagementApi/src/DbManagementController.cs
+++ AD.DatabaseManagementApi/src/DbManagementController.cs
@@ -7,15 +7,16 @@ using JetBrains.Annotations;
 namespace AD.DatabaseManagementApi
 {
     [PublicAPI]
-    public class DbManagementController : IDisposable
+    public class DbManagementController
     {
         private readonly DbContext _context;
 
-        private readonly IList<DbManagementServiceContainer> _services = new List<DbManagementServiceContainer>();
+        private readonly IList<DbManagementServiceContainer> _services;
 
-        public DbManagementController(DbContext context) 
+        public DbManagementController(DbContext context, IEnumerable<Func<DbManagementServiceContainer>> services) 
         {
             _context = context;
+            _services = services.Select(x => x()).ToList();
         }
 
         public void Invoke()
@@ -24,92 +25,25 @@ namespace AD.DatabaseManagementApi
             DateTime currentTime = DateTime.Now;
             Console.WriteLine(); 
             Console.WriteLine($@"Executing services at {startTime.ToShortTimeString()}. Please wait...");
-            for (int i = 0; i < _services.Count; i++)
+            int index = 1;
+            foreach (DbManagementServiceContainer service in _services)
             {
-                DbManagementServiceContainer service = _services[i];
                 Console.WriteLine();
-                Console.WriteLine($@"> Starting service {i + 1} of {_services.Count} at {currentTime}: {service.Name}.");
+                Console.WriteLine($@"> Starting service {index++} of {_services.Count} at {currentTime}: {service.Name}.");
+
                 int count = service.Invoke(_context);
+
                 Console.WriteLine($@"> {service.Name} completed with {count} records affected in {(DateTime.Now - currentTime).TotalMinutes:0.00} minutes.");
+
                 currentTime = DateTime.Now;
             }
+
             Console.WriteLine();
             Console.WriteLine($@"Completed {_services.Count} services in {(DateTime.Now - startTime).TotalMinutes:0.00} minutes. Press enter to exit...");
+
             while (Console.ReadKey().Key != ConsoleKey.Enter)
             {
             }
-        }
-
-        public void Message(string message, DbManagementServiceContainer serviceContainer)
-        {
-            string response = null;
-            while (TestResponse(response))
-            {
-                Console.WriteLine();
-                Console.WriteLine($@"{message} [Y/N]");
-                response = Console.ReadLine();
-                if (!response?.Equals("Y", StringComparison.OrdinalIgnoreCase) ?? false)
-                {
-                    continue;
-                }
-                serviceContainer.Parameters = HandleParameters();
-                _services.Add(serviceContainer);
-            }
-        }
-
-        public void Message(string message, string year, DbManagementServiceContainer serviceContainer)
-        {
-            string response = null;
-            while (TestResponse(response))
-            {
-                Console.WriteLine();
-                Console.WriteLine(message);
-                response = Console.ReadLine();
-                if (!response?.Equals("Y", StringComparison.OrdinalIgnoreCase) ?? false)
-                {
-                    continue;
-                }
-                IDictionary<string, string> parameters = new Dictionary<string, string>();
-                parameters.Add("year", year);
-                serviceContainer.Parameters = parameters;
-                _services.Add(serviceContainer);
-            }
-        }
-
-        private IDictionary<string, string> HandleParameters()
-        {
-            IDictionary<string, string> parameters = new Dictionary<string, string>();
-            string response = null;
-            while (TestResponse(response))
-            {
-                Console.WriteLine();
-                Console.WriteLine(@"Does this service require parameters? [Y/N]");
-                response = Console.ReadLine();
-                if (!response?.Equals("Y", StringComparison.OrdinalIgnoreCase) ?? false)
-                {
-                    continue;
-                }
-                Console.WriteLine();
-                Console.WriteLine(@"Enter parameters in the form: [name1 = value1, value2, value3; name2 = value1]");
-                string input = Console.ReadLine();
-                parameters = input?.Replace(" ", null)
-                                  .Split(';')
-                                  .Select(x => x.Split('='))
-                                  .ToDictionary(x => x[0], x => x[1]);
-            }
-            return parameters;
-        }
-
-        void IDisposable.Dispose()
-        {
-            _context.Dispose();
-        }
-
-        private bool TestResponse(string response)
-        {
-            response = response ?? "";
-            return !response.Equals("Y", StringComparison.OrdinalIgnoreCase)
-                && !response.Equals("N", StringComparison.OrdinalIgnoreCase);
         }
     }
 }
---
+++ AD.DatabaseManagementApi/src/DbManagementControllerBuilder.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using JetBrains.Annotations;
+
+namespace AD.DatabaseManagementApi
+{
+    [PublicAPI]
+    public class DbManagementControllerBuilder
+    {
+        private readonly IList<Func<DbManagementServiceContainer>> _messages = new List<Func<DbManagementServiceContainer>>();
+
+        public DbManagementControllerBuilder()
+        {
+        }
+
+        public DbManagementController Build(DbContext context)
+        {
+            return new DbManagementController(context, _messages);
+        }
+
+
+        public Func<DbManagementServiceContainer> AddMessage(string message, DbManagementServiceContainer serviceContainer)
+        {
+            Func<DbManagementServiceContainer> action =  () =>
+            {
+                string response = null;
+                while (TestResponse(response))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($@"{message} [Y/N]");
+                    response = Console.ReadLine();
+                    if (!response?.Equals("Y", StringComparison.OrdinalIgnoreCase) ?? false)
+                    {
+                        continue;
+                    }
+                    serviceContainer.Parameters = HandleParameters();
+                    break;
+                }
+                return serviceContainer;
+            };
+            return action;
+        }
+
+        public Func<DbManagementServiceContainer> AddMessage(string message, string year, DbManagementServiceContainer serviceContainer)
+        {
+            Func<DbManagementServiceContainer> action = () =>
+            {
+                string response = null;
+                while (TestResponse(response))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(message);
+                    response = Console.ReadLine();
+                    if (!response?.Equals("Y", StringComparison.OrdinalIgnoreCase) ?? false)
+                    {
+                        continue;
+                    }
+                    IDictionary<string, string> parameters = new Dictionary<string, string>();
+                    parameters.Add("year", year);
+                    serviceContainer.Parameters = parameters;
+                    break;
+                }
+                return serviceContainer;
+            };
+            return action;
+        }
+
+        private IDictionary<string, string> HandleParameters()
+        {
+            IDictionary<string, string> parameters = new Dictionary<string, string>();
+            string response = null;
+            while (TestResponse(response))
+            {
+                Console.WriteLine();
+                Console.WriteLine(@"Does this service require parameters? [Y/N]");
+                response = Console.ReadLine();
+                if (!response?.Equals("Y", StringComparison.OrdinalIgnoreCase) ?? false)
+                {
+                    continue;
+                }
+                Console.WriteLine();
+                Console.WriteLine(@"Enter parameters in the form: [name1 = value1, value2, value3; name2 = value1]");
+                string input = Console.ReadLine();
+                parameters = input?.Replace(" ", null)
+                                  .Split(';')
+                                  .Select(x => x.Split('='))
+                                  .ToDictionary(x => x[0], x => x[1]);
+            }
+            return parameters;
+        }
+
+        private bool TestResponse(string response)
+        {
+            response = response ?? "";
+            return !response.Equals("Y", StringComparison.OrdinalIgnoreCase)
+                && !response.Equals("N", StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
