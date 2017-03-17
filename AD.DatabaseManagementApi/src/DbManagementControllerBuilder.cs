4104f5a549f8524b5d7a57b8839db36f1ef8588b
austin.drenski <austin.drenski@gmail.com>
2017-03-17 13:35:58 -0400

    .

--- AD.DatabaseManagementApi/src/DbManagementControllerBuilder.cs
+++ AD.DatabaseManagementApi/src/DbManagementControllerBuilder.cs
@@ -6,19 +6,29 @@ using System.Collections.Immutable;
 
 namespace AD.DatabaseManagementApi
 {
+    /// <summary>
+    /// Builds an immutable <see cref="DbManagementController"/>.
+    /// </summary>
     [PublicAPI]
     public class DbManagementControllerBuilder
     {
         private readonly IList<Func<IDbManagementService>> _messages = new List<Func<IDbManagementService>>();
 
+        /// <summary>
+        /// Constructs an immutable <see cref="DbManagementController"/> from this <see cref="DbManagementControllerBuilder"/>.
+        /// </summary>
+        /// <returns></returns>
         [NotNull]
         public DbManagementController Build()
         {
             return new DbManagementController(_messages.Select(x => x()).Where(x => x != null).ToImmutableArray());
         }
 
+        /// <summary>
+        /// Adds a message and function to the builder. The function should return an <see cref="IDbManagementService"/>.
+        /// </summary>
         [NotNull]
-        public DbManagementControllerBuilder AddMessage([NotNull] string message, [NotNull] IDbManagementService service)
+        public DbManagementControllerBuilder AddMessage([NotNull] string message, [NotNull] Func<IDbManagementService> service)
         {
             Func<IDbManagementService> action = () =>
             {
@@ -29,7 +39,7 @@ namespace AD.DatabaseManagementApi
                     string response = Console.ReadLine() ?? "";
                     if (response.Equals("Y", StringComparison.OrdinalIgnoreCase))
                     {
-                        return service;
+                        return service();
                     }
                     if (response.Equals("N", StringComparison.OrdinalIgnoreCase))
                     {
